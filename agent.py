9cd8180817f7ca2233d8cf819cce503960651d2f
Malika <alinurbe@gmail.com>
2018-01-25 23:50:29 -0500

    knuckles are off to good start

---
+++ agent.py
@@ -0,0 +1,6 @@
+import os
+#for filename in os.listdir("."):
+ #   if (filename == "knuckles.go"):
+teamName = "knuckles.go"
+if teamName in os.listdir("."):
+        print("knuckles succeeded show da wei")
