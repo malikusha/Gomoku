50b57ea44ed8e34e803b8637e67278497eccd027
Malika <alinurbe@gmail.com>
2018-01-26 23:18:43 -0500

    closing a file

--- agent.py
+++ agent.py
@@ -5,14 +5,17 @@ teamName = "knuckles"
 
 def init():
     if teamName+".go" in os.listdir(".") and "end_game" not in os.listdir("."):
-        move = open("move_file").read()                         # move is the opponent's move
-        if (move):                                              # if the move_file is empty, you are playing with white (move = 0)
-            open("move_file", 'w').write(minimax(move))
-        else:
-            open("move_file", 'w').write(minimax(0))
+        move_file = open("move_file")
+        move = move_file.read()                                   # move is the opponent's move
+        move_file.close()
+        if not move: move = 0                                     # if the move_file is empty, you are playing with white (move = 0)
+        f = open("move_file", 'w').write(minimax(move))
+        f.close()
+
     return
 
 def minimax(move):
+
     return teamName+" "+"E"+" "+"4"
 
 init()
