fc5b01fd42e6b2977e4595b212b7d841d805d7a1
Malika <alinurbe@gmail.com>
2018-01-26 23:21:18 -0500

    quick fix

--- agent.py
+++ agent.py
@@ -9,7 +9,8 @@ def init():
         move = move_file.read()                                   # move is the opponent's move
         move_file.close()
         if not move: move = 0                                     # if the move_file is empty, you are playing with white (move = 0)
-        f = open("move_file", 'w').write(minimax(move))
+        f = open("move_file", 'w')
+        f.write(minimax(move))
         f.close()
 
     return
