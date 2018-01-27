411f60a57ccfdace1b34930590eb881230a7eb2c
Malika <alinurbe@gmail.com>
2018-01-26 22:47:44 -0500

    reading from and writing into the move_file

--- agent.py
+++ agent.py
@@ -1,6 +1,24 @@
 import os
-#for filename in os.listdir("."):
- #   if (filename == "knuckles.go"):
-teamName = "knuckles.go"
-if teamName in os.listdir("."):
-        print("knuckles succeeded show da wei")
+import scipy.io
+
+teamName = "knuckles"
+
+def init():
+    if teamName+".go" in os.listdir(".") and "end_game" not in os.listdir("."):
+        move = open("move_file").read()                         # move is the opponent's move
+        if (move):                                              # if the move_file is empty, you are playing with white (move = 0)
+            open("move_file", 'w').write(minimax(move))
+        else:
+            open("move_file", 'w').write(minimax(0))
+    return
+
+def minimax(move):
+    return teamName+" "+"E"+" "+"4"
+
+init()
+
+
+
+
+
+
