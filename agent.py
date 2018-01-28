fd831fbec6627718c8c1d429caae4bd7dcf20e6f
Bhon Bunnag <bhonris@gmail.com>
2018-01-28 11:08:30 -0500

    Broken Stuff

--- agent.py
+++ agent.py
@@ -1,22 +1,26 @@
 import os
-import scipy.io
+import time
+#import scipy.io
 
 teamName = "knuckles"
 
 def init():
-    if teamName+".go" in os.listdir(".") and "end_game" not in os.listdir("."):
-        move = open("move_file").read()                         # move is the opponent's move
-        if (move):                                              # if the move_file is empty, you are playing with white (move = 0)
-            open("move_file", 'w').write(minimax(move))
-        else:
-            open("move_file", 'w').write(minimax(0))
+    move = open("move_file").read()                         # move is the opponent's move
+    if (move):                                              # if the move_file is empty, you are playing with white (move = 0)
+        open("move_file", 'w').write(minimax(move))
+    else:
+        open("move_file", 'w').write(minimax(0))
     return
 
 def minimax(move):
     return teamName+" "+"E"+" "+"4"
+print("Agent py called")
 
-init()
-
+while(1==1):
+  while((teamName+".go" not in os.listdir(".")) and ("end_game" not in os.listdir("."))):
+    pass
+  init()
+  time.sleep(4)
 
 
 
