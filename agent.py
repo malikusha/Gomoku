03d54a311cc7c9402baaf73b1cc740a03839203f
Bhon Bunnag <bhonris@gmail.com>
2018-01-28 11:21:02 -0500

    Fixed small bugs

--- agent.py
+++ agent.py
@@ -1,17 +1,15 @@
-import osimport numpy as np
+import os
 import time
 
 
 
 teamName = "knuckles"
-board = np.zeros((15, 15))
 validMoves = []
 for x in range(0,15):
     for y in range(0,15):
         validMoves.append([x, y])
 columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 'M', 'N', 'O', 'P', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-print(board)
 
 def init():
     move = open("move_file").read()                         # move is the opponent's move
